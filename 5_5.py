ac773893bf0d58028bda5a946624ecd38e1ddabb
JmeLahman <lahmanja@protonmail.com>
2018-11-26 21:17:36 -0800

    finished remaining problems

---
+++ 5_5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Chapter 5 Problem #5: Estimating the Area of a Circle
+
+As a part of this challenge, write a program that will find the estimated area
+of a circle, given any radius, using this approach. The program should print 
+the estimated area of the circle for three different values of the number of
+darts: 10^3, 10^5, and 10^6. Here's a sample output of the completed solution:
+    
+Radius: 2
+Area: 12.566370614359172, Estimated (1000 darts): 12.576
+Area: 12.566370614359172, Estimated (100000 darts): 12.58176
+
+Doing Math with Python by Amit Saha
+
+Solution by Jamison Lahman, November 26, 2018
+"""
+import math
+import random
+
+def estimate_area(r, n):
+    square_area = 4*r**2
+    count = 0
+    for i in range(n):
+        p = (random.uniform(-r,r),random.uniform(-r,r))
+        if math.sqrt(p[0]**2+p[1]**2) <= r:
+            count += 1
+    return count/n * square_area
+
+if __name__ == '__main__':
+    darts = [1000,100000,1000000]
+
+    r = float(input('Radius: '))
+
+    for n in darts:
+        area = estimate_area(r, n)
+        print('Area: {0}, Estimated ({1} darts): {2}'.format(math.pi*r**2,n,area))
---
+++ 6_4.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Chapter 6 Problem #4: Drawing the Mandelbrot Set
+
+Your challenge here is to write a program to draw the Mandelbrot Set.
+
+Doing Math with Python by Amit Saha
+
+Solution by Jamison Lahman, November 26, 2018
+"""
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random
+
+def initialize_image(x_p, y_p):
+    image = []
+    for i in range(y_p):
+        x_colors = []
+        for j in range(x_p):
+            x_colors.append(0)
+        image.append(x_colors)
+    return image
+
+def color_mandelbrot():
+    x0, x1 = -2.5,1.0
+    y0, y1 = -1.0,1.0
+    x_p = 400
+    y_p = 400
+    max_iteration = 1000
+    
+    image = initialize_image(x_p, y_p)
+    
+    dx = (x1-x0)/(x_p-1)
+    dy = (y1-y0)/(y_p-1)
+    x = [x0 + i*dx for i in range(x_p)]
+    y = [y0 + i*dy for i in range(y_p)]
+  
+    
+    for k, xk in enumerate(x):
+        for i, yi in enumerate(y):
+            z = complex(0,0)
+            c = complex(xk,yi)
+            iteration = 0
+            while (iteration < max_iteration and abs(z) < 2): 
+                iteration += 1
+                z = z**2+c
+            
+            image[i][k] = iteration
+    
+    plt.imshow(image,origin='lower', extent=(x0,x1,y0,y1), cmap=cm.Greys_r,
+               interpolation='nearest')
+    plt.colorbar()
+    plt.show()
+    
+if __name__ == '__main__':
+    color_mandelbrot()
